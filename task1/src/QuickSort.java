d4cb0767b0c822a8fbc85fdb8b08cb6980964333
erybakova <62673770+erybakova@users.noreply.github.com>
2023-01-17 00:21:43 +0300

    FIX: task1: changed code style to camelCase

--- task1/src/QuickSort.java
+++ task1/src/QuickSort.java
@@ -2,23 +2,23 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class QuickSort <T extends Comparable<T>> {
-    public static int count_cmp_ = 0;
+public class QuickSort {
+    public static int countCmp = 0;
 
-    public static <T> void Sort(T[] a, Comparator<T> cmp) {
-        QuickSortImpl(a, 0, a.length, cmp);
+    public static <T> void sort(T[] a, Comparator<T> cmp) {
+        _quickSortImpl(a, 0, a.length, cmp);
     }
 
-    public static <T> void QuickSortImpl(T[] a, int left, int right, Comparator<T> cmp) {
+    private static <T> void _quickSortImpl(T[] a, int left, int right, Comparator<T> cmp) {
         if (left < right) {
-            int pivot = Partition(a, left, right, cmp);
+            int pivot = _partition(a, left, right, cmp);
 
-            QuickSortImpl(a, left, pivot, cmp);
-            QuickSortImpl(a, pivot + 1, right, cmp);
+            _quickSortImpl(a, left, pivot, cmp);
+            _quickSortImpl(a, pivot + 1, right, cmp);
         }
     }
 
-    public static <T> int Partition(T[] a, int left, int right, Comparator<T> cmp) {
+    private static <T> int _partition(T[] a, int left, int right, Comparator<T> cmp) {
         T x = a[left];
         T y = a[right - 1];
         int length = right - left;
@@ -28,7 +28,7 @@ public class QuickSort <T extends Comparable<T>> {
         else
             mid = a[left + (length / 2)];
 
-        T pivot = MedianOf3(x, y, mid, cmp);
+        T pivot = _medianOf3(x, y, mid, cmp);
         int piv_ind = java.util.Arrays.asList(a).indexOf(pivot);
 
         a[piv_ind] = a[left];
@@ -38,16 +38,16 @@ public class QuickSort <T extends Comparable<T>> {
 
         for (int j = left + 1; j < right; j++) {
             if (cmp.compare(a[j], pivot) < 0) {
-                Swap(a, i, j);
+                _swap(a, i, j);
                 i++;
             }
         }
-        Swap(a, left, i - 1);
+        _swap(a, left, i - 1);
 
         return i - 1;
     }
 
-    public static <T> T MedianOf3(T x, T y, T z, Comparator<T> cmp) {
+    private static <T> T _medianOf3(T x, T y, T z, Comparator<T> cmp) {
         if (cmp.compare(x, y) <= 0 && cmp.compare(y, z) <= 0)
             return y;  // a b c
         if (cmp.compare(x, z) <= 0 && cmp.compare(z, y) <= 0)
@@ -61,7 +61,7 @@ public class QuickSort <T extends Comparable<T>> {
         return y;  // c b a
     }
 
-    public static <T> void Swap(T[] a, int i, int j) {
+    private static <T> void _swap(T[] a, int i, int j) {
         T tmp = a[i];
         a[i] = a[j];
         a[j] = tmp;
@@ -72,14 +72,14 @@ public class QuickSort <T extends Comparable<T>> {
 
         Comparator<Integer> cmp = new Comparator<Integer>() {
             public int compare(Integer i, Integer j) {
-                count_cmp_++;
+                countCmp++;
                 return i.compareTo(j);
             }
         };
 
         System.out.println(Arrays.toString(a));
-        Sort(a, cmp);
+        sort(a, cmp);
         System.out.println(Arrays.toString(a));
-        System.out.println(count_cmp_);
+        System.out.println(countCmp);
     }
 }

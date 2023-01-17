b85a07ee2d44155173130e2b44b5ac7a6df027c6
erybakova <62673770+erybakova@users.noreply.github.com>
2023-01-17 14:04:34 +0300

    NEW: task0: created file with JUnit tests

---
+++ task0/src/test/java/BinarySearchTest.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class BinarySearchTest {
+    @Test
+    @DisplayName("Test BinarySearch.find")
+    public void testBinarySearch() {
+      
+    }
+}

22dd7b0e65a1469c316ec7be45d59464a37bc6be
erybakova <62673770+erybakova@users.noreply.github.com>
2023-01-18 01:18:44 +0300

    FIX: task1: minor fixes in tests

--- task1/src/test/java/QuickSortTest.java
+++ task1/src/test/java/QuickSortTest.java
@@ -1,7 +1,8 @@
-import java.io.*;
 import java.util.Arrays;
+import java.util.Random;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 public class QuickSortTest {
@@ -9,26 +10,11 @@ public class QuickSortTest {
     @DisplayName("Test QuickSort.sort for 10 Integer()'s")
     public void testQuickSortInteger10() {
         Integer [] a = { 10, 10, 4, 3, 2, 1, 0, -2, 125, -10 };
-        System.out.println(Arrays.toString(a));
 
-        ClassComparator cmp = new ClassComparator<Integer>();
+        SuperComparator<Integer> cmp = new SuperComparator<Integer>();
         QuickSort.sort(a, cmp);
 
-        System.out.println(Arrays.toString(a));
-        System.out.println(cmp.getCountCmp());
-    }
-
-    @Test
-    @DisplayName("Test Arrays.sort of 10 Integer()'s")
-    public void testStandardSortInteger10() {
-        Integer [] a = { 10, 10, 4, 3, 2, 1, 0, -2, 125, -10 };
-        System.out.println(Arrays.toString(a));
-
-        ClassComparator cmp = new ClassComparator<Integer>();
-        Arrays.sort(a, cmp);
-
-        System.out.println(Arrays.toString(a));
-        System.out.println(cmp.getCountCmp());
+        Assertions.assertTrue(QuickSort.isSorted(a, cmp));
     }
 
     @Test
@@ -38,29 +24,11 @@ public class QuickSortTest {
 
         for (int i = 0; i < 100; ++i)
             a[i] = 5;
-        System.out.println(Arrays.toString(a));
 
-        ClassComparator cmp = new ClassComparator<Integer>();
+        SuperComparator<Integer> cmp = new SuperComparator<Integer>();
         QuickSort.sort(a, cmp);
 
-        System.out.println(Arrays.toString(a));
-        System.out.println(cmp.getCountCmp());
-    }
-
-    @Test
-    @DisplayName("Test Arrays.sort for 100 equal Integer()'s")
-    public void testStandardSortIntegerEqual() {
-        Integer [] a = new Integer[100];
-
-        for (int i = 0; i < 100; ++i)
-            a[i] = 5;
-        System.out.println(Arrays.toString(a));
-
-        ClassComparator cmp = new ClassComparator<Integer>();
-        Arrays.sort(a, cmp);
-
-        System.out.println(Arrays.toString(a));
-        System.out.println(cmp.getCountCmp());
+        Assertions.assertTrue(QuickSort.isSorted(a, cmp));
     }
 
     @Test
@@ -70,29 +38,11 @@ public class QuickSortTest {
 
         for (int i = 0; i < 100; ++i)
             a[i] = i + 1;
-        System.out.println(Arrays.toString(a));
 
-        ClassComparator cmp = new ClassComparator<Integer>();
+        SuperComparator<Integer> cmp = new SuperComparator<Integer>();
         QuickSort.sort(a, cmp);
 
-        System.out.println(Arrays.toString(a));
-        System.out.println(cmp.getCountCmp());
-    }
-
-    @Test
-    @DisplayName("Test Arrays.sort for 100 sorted Integer()'s")
-    public void testStandardSortIntegerSorted() {
-        Integer [] a = new Integer[100];
-
-        for (int i = 0; i < 100; ++i)
-            a[i] = i + 1;
-        System.out.println(Arrays.toString(a));
-
-        ClassComparator cmp = new ClassComparator<Integer>();
-        Arrays.sort(a, cmp);
-
-        System.out.println(Arrays.toString(a));
-        System.out.println(cmp.getCountCmp());
+        Assertions.assertTrue(QuickSort.isSorted(a, cmp));
     }
 
     @Test
@@ -102,70 +52,38 @@ public class QuickSortTest {
 
         for (int i = 0; i < 100; ++i)
             a[i] = 100 - i;
-        System.out.println(Arrays.toString(a));
 
-        ClassComparator cmp = new ClassComparator<Integer>();
+        SuperComparator<Integer> cmp = new SuperComparator<Integer>();
         QuickSort.sort(a, cmp);
 
-        System.out.println(Arrays.toString(a));
-        System.out.println(cmp.getCountCmp());
+        Assertions.assertTrue(QuickSort.isSorted(a, cmp));
     }
 
-    @Test
-    @DisplayName("Test Arrays.sort for 100 inverse sorted Integer()'s")
-    public void testStandardSortIntegerInvSorted() {
-        Integer [] a = new Integer[100];
-
-        for (int i = 0; i < 100; ++i)
-            a[i] = 100 - i;
-        System.out.println(Arrays.toString(a));
-
-        ClassComparator cmp = new ClassComparator<Integer>();
-        Arrays.sort(a, cmp);
-
-        System.out.println(Arrays.toString(a));
-        System.out.println(cmp.getCountCmp());
+    void initArrayOfIntegerRandom(Integer [] a) {
+        Random random = new Random(123);
+        for (int i = 0; i < a.length; ++i)
+            a[i] = random.nextInt();
     }
 
-    public void testQuickSortInteger(int n) {
+    public void testSortingComparison(int n) {
         Integer [] a = new Integer[n];
 
-        for (int i = 0; i < n; ++i)
-            a[i] = (int)(Math.random() * 201) - 100; // int from [-100; +100]
-        //System.out.println(Arrays.toString(a));
+        SuperComparator<Integer> cmp = new SuperComparator<Integer>();
 
-        ClassComparator cmp = new ClassComparator<Integer>();
+        initArrayOfIntegerRandom(a);
         QuickSort.sort(a, cmp);
+        Assertions.assertTrue(QuickSort.isSorted(a, cmp));
+        System.out.println("Number of comparisons on array of size " + n + " (QuickSort.sort): " + cmp.getCountCmp());
 
-        //System.out.println(Arrays.toString(a));
-        System.out.println(cmp.getCountCmp());
-    }
-
-    public void testStandardSortInteger(int n) {
-        Integer [] a = new Integer[n];
-
-        for (int i = 0; i < n; ++i)
-            a[i] = (int)(Math.random() * 201) - 100; // int from [-100; +100]
-        //System.out.println(Arrays.toString(a));
-
-        ClassComparator cmp = new ClassComparator<Integer>();
+        initArrayOfIntegerRandom(a);
         Arrays.sort(a, cmp);
-
-        //System.out.println(Arrays.toString(a));
-        System.out.println(cmp.getCountCmp());
+        System.out.println("Number of comparisons on array of size " + n + " (Arrays.sort): " + cmp.getCountCmp() + "\n");
     }
 
     @Test
-    @DisplayName("Test QuickSort.sort for 10, 100, ..., 10^6 Integer()'s")
-    public void testQuickSortIntegerAll() {
+    @DisplayName("Test QuickSort.sort and Arrays.sort for 10, 100, ..., 10^6 Integer()'s")
+    public void testQuickSortBig() {
         for (int n = 1; n <= 1000000; n *= 10)
-            testQuickSortInteger(n);
-    }
-
-    @Test
-    @DisplayName("Test Arrays.sort for 10, 100, ..., 10^6 Integer()'s")
-    public void testStandardSortIntegerAll() {
-        for (int n = 1; n <= 1000000; n *= 10)
-            testStandardSortInteger(n);
+            testSortingComparison(n);
     }
 }

aef7d72fecec15f2748b0e5c487cc94a4fd1a366
erybakova <62673770+erybakova@users.noreply.github.com>
2023-01-17 23:31:15 +0300

    ENH: task0: added tests

--- task0/src/test/java/BinarySearchTest.java
+++ task0/src/test/java/BinarySearchTest.java
@@ -1,13 +1,49 @@
 import java.io.*;
 import java.util.Arrays;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 public class BinarySearchTest {
     @Test
-    @DisplayName("Test BinarySearch.find")
-    public void testBinarySearch() {
-      
+    @DisplayName("Test BinarySearch.find for array of size 10")
+    public void testBinarySearchInEvenSizeArray() {
+        int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        for (int i = 1; i <= a.length; ++i) {
+            int res = BinarySearch.find(a, i);
+            assertTrue(res >= 0);
+            assertEquals(res, i - 1);
+        }
+    }
+
+    @Test
+    @DisplayName("Test BinarySearch.find for array of size 11")
+    public void testBinarySearchInOddSizeArray() {
+        int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+        for (int i = 1; i <= a.length; ++i) {
+            int res = BinarySearch.find(a, i);
+            assertTrue(res >= 0);
+            assertEquals(res, i - 1);
+        }
+    }
+
+    @Test
+    @DisplayName("Test BinarySearch.find on missing items")
+    public void testBinarySearchOnMissingItems() {
+        int a[] = { 1, 2, 3, 4, 5, 6, 7, 9, 10, 11 };
+        int res;
+
+        res = BinarySearch.find(a, 0);
+        assertTrue(res < 0);
+
+        res = BinarySearch.find(a, 12);
+        assertTrue(res < 0);
+
+        res = BinarySearch.find(a, 8);
+        assertTrue(res < 0);
     }
 }
